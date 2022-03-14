d64a965d0ed3421c6626ad544ca0a9942400dcf6
yayanrw <yayanraw@gmail.com>
2022-03-14 13:41:08 +0700

    add timezone

--- application/config/config.php
+++ application/config/config.php
@@ -488,7 +488,8 @@ $config['compress_output'] = FALSE;
 | helper' page of the user guide for information regarding date handling.
 |
 */
-$config['time_reference'] = 'local';
+$config['time_reference'] = 'Asia/Jakarta';
+date_default_timezone_set("Asia/Jakarta");
 
 /*
 |--------------------------------------------------------------------------
