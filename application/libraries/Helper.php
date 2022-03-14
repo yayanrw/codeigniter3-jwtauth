dff9bc2e6effeeaaa286c4c29d9fec019497b13e
yayanrw <yayanraw@gmail.com>
2022-03-14 15:08:57 +0700

    add hel[er

---
+++ application/libraries/Helper.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Helper extends CI_Model
+{
+    public function Authorize()
+    {
+        $headers = $this->input->request_headers();
+        if (array_key_exists('Authorization', $headers) && !empty($headers['Authorization'])) {
+            $decodedToken = AUTHORIZATION::validateTimestamp($headers['Authorization']);
+
+            if ($decodedToken == false) {
+                $this->output
+                    ->set_content_type('application/json')
+                    ->set_status_header(401);
+                echo "Unauthorized";
+                exit;
+            }
+        }
+    }
+}
