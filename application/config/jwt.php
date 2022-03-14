2cfcc9557a4a5bbaa9aa043e9d93aebaeb2387d1
yayanrw <yayanraw@gmail.com>
2022-03-14 10:58:08 +0700

    jwt key

---
+++ application/config/jwt.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['jwt_key'] = 'aYyWWEzd3aZOfjwOUEitwrFbATE21Z84';
+
+/*Generated token will expire in 1 minute for sample code
+* Increase this value as per requirement for production
+*/
+$config['token_timeout'] = 1;
+
+/* End of file jwt.php */
+/* Location: ./application/config/jwt.php */
