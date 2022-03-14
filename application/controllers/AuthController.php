8b33cac00b7d80d1a4741738c865fb3e007daa70
yayanrw <yayanraw@gmail.com>
2022-03-14 13:41:25 +0700

    add index get

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -38,6 +38,29 @@ class AuthController extends REST_Controller
             ]);
         }
     }
+
+    public function index_get()
+    {
+        try {
+            $headers = $this->input->request_headers();
+            if (array_key_exists('Authorization', $headers) && !empty($headers['Authorization'])) {
+                $decodedToken = AUTHORIZATION::validateTimestamp($headers['Authorization']);
+
+                // return response if token is valid
+                if ($decodedToken != false) {
+                    $this->set_response($decodedToken, REST_Controller::HTTP_OK);
+                    return;
+                }
+            }
+
+            $this->set_response("Unauthorized", REST_Controller::HTTP_UNAUTHORIZED);
+        } catch (\Throwable $th) {
+            echo json_encode([
+                'status' => false,
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
 }
 
 /* End of file AuthController.php */
