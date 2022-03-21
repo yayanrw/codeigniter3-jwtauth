29499b2405e4ef038f9cb38a451d714f4c872cab
yayanrw <yayanraw@gmail.com>
2022-03-21 09:07:22 +0700

    use natural code

--- application/libraries/Helper.php
+++ application/libraries/Helper.php
@@ -1,26 +1,30 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Helper extends CI_Model
+class Helper
 {
     public function Authorize()
     {
-        $headers = $this->input->request_headers();
+        $headers = apache_request_headers();
         if (array_key_exists('Authorization', $headers) && !empty($headers['Authorization'])) {
             $decodedToken = AUTHORIZATION::validateTimestamp($headers['Authorization']);
 
             if ($decodedToken == false) {
-                $this->output
-                    ->set_content_type('application/json')
-                    ->set_status_header(401);
-                echo "Token Expired";
+                header("Content-type: application/json; charset=utf-8");
+                http_response_code(401);
+                echo json_encode(array(
+                    'status' => false,
+                    'message' => 'Token is invalid'
+                ));
                 exit;
             }
         } else {
-            $this->output
-                ->set_content_type('application/json')
-                ->set_status_header(401);
-            echo "Unauthorized";
+            header("Content-type: application/json; charset=utf-8");
+            http_response_code(401);
+            echo json_encode(array(
+                'status' => false,
+                'message' => 'Unauthorized'
+            ));
             exit;
         }
     }
